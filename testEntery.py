78d8d1d9034959839b9b35465942f0892f72be91
steamtrail241 <72204170+steamtrail241@users.noreply.github.com>
2022-07-17 19:07:28 -0400

    input regular prototype complete

--- testEntery.py
+++ testEntery.py
@@ -1,33 +1,6 @@
 from tkinter import Button, Entry, StringVar, Tk
 
-asjkdfhk = 0
 
-class mywindow(Tk):
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        self.myEnteryText = StringVar(self, value="20")
-        self.myEntery = Entry(self, textvariable=self.myEnteryText)
-        self.myEntery.grid(row=0, column=0)
-        self.mybutton = Button(self, padx=10, pady=10)
-        self.mybutton.bind("<Button-1>", self.thingy)
-        self.mybutton.grid(row=0, column=1)
-        self.updater()
-        self.mainloop()
-    
-    def thingy(self, event=None):
-        self.myEntery.delete(0, 100)
-        self.myEntery.insert(-1, str(asjkdfhk))
-    
-    def updater(self, event=None):
-        change()
-        self.after(1000, self.updater)
-
-
-def change(event=None):
-    global asjkdfhk
-    asjkdfhk += 1
-
-hasjkdf = mywindow()
 
 class Button1():
     def __init__(self, root, row, column, len, wid, text=" ", color="white", fg="black", leftClick=None, Enter=None, Leave=None):
@@ -68,23 +41,110 @@ class Button1():
         self.button.grid(row=self.row+1, column=self.column)
 
 class Inputer():
-    def __init__(self, root, row, column, padx, pady, defaultText):
+    def __init__(self, root, row, column, padx, pady, defaultText, limit):
         self.root = root
         self.row = row
         self.column = column
         self.x = padx
         self.y = pady
-        self.contains = defaultText
+        self.contains = int(defaultText)
+        self.limit = limit
 
         self.entry = Entry(self.root)
-        self.entry.insert(self.contains)
-        self.entry.grid(row=self.row, column=self.column)
+
+        self.leftChange10 = Button1(
+            root,
+            row-1, column,
+            16, 4,
+            text="<<<",
+            leftClick=lambda event: self.changeAmount(-10)
+        )
+
+        self.leftChange5 = Button1(
+            root, 
+            row-1, column + 1,
+            16, 4,
+            text="<<",
+            leftClick=lambda event: self.changeAmount(-5)
+        )
+
+        self.leftChange1 = Button1(
+            root,
+            row-1, column + 2,
+            16, 4,
+            text="<",
+            leftClick=lambda event: self.changeAmount(-1)
+        )
+
+        self.rightChange1 = Button1(
+            root,
+            row-1, column + 4,
+            16, 4,
+            text=">",
+            leftClick=lambda event: self.changeAmount(1)
+        )
+
+        self.rightChange5 = Button1(
+            root,
+            row-1, column + 5,
+            16, 4,
+            text=">>",
+            leftClick=lambda event: self.changeAmount(5)
+        )
+
+        self.rightChange10 = Button1(
+            root,
+            row-1, column + 6,
+            16, 4,
+            text=">>>",
+            leftClick=lambda event: self.changeAmount(10)
+        )
+
+        self.entry.insert(-1, self.contains)
+        self.entry.grid(row=self.row, column=self.column+3)
     
     def update(self):
         self.entry.grid_remove()
         self.entry = Entry(self.root)
-        self.entry.insert(self.contains)
+        self.entry.insert(str(self.contains))
         self.entry.grid(row=self.row, column=self.column)
     
-    def changeAmount(self, event=None):
-        pass
+    def changeAmount(self, amount):
+        test = self.contains + amount
+        if test > self.limit:
+            self.contains = self.limit
+        elif test < 0:
+            self.contains = 0
+        else:
+            self.contains = test
+        print("changed by:"+str(amount))
+        self.entry.delete(0, 100)
+        self.entry.insert(-1, str(self.contains))
+    
+    def thingy(self):
+        print("comand was called")
+
+
+asjkdfhk = 0
+
+class mywindow(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.kasdfkjhj = Inputer(self, 0, 0, 16, 4, "20", 100)
+        self.updater()
+        self.mainloop()
+    
+    def thingy(self, event=None):
+        self.myEntery.delete(0, 100)
+        self.myEntery.insert(-1, str(asjkdfhk))
+    
+    def updater(self, event=None):
+        change()
+        self.after(1000, self.updater)
+
+
+def change(event=None):
+    global asjkdfhk
+    asjkdfhk += 1
+
+hasjkdf = mywindow()
